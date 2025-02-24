2e92cce6b1ee8ef6c69bd6e85ce92099e07172d3
Kartikbca <kartikahir577@gmail.com>
2025-02-24 09:09:22 +0530

    Add files via upload

---
+++ Ch1/ch2p2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class ch2p2
+{
+    public static void main(String args[])
+    {
+        int x;
+        double i,emi;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter loan amount ::");
+        x=sc.nextInt();
+        System.out.println("Enter intrest ::");
+        i=sc.nextDouble();
+        emi=x*i/(100*60);
+        System.out.println("The emi ::"+emi);
+    }
+}
